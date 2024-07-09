b929b970e9e8b1dd3f8fa3189ef4ed9285f6d01a
edoup <edoumps@gmail.com>
2024-07-09 07:57:37 +0100

    Added comment in the javascript file.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // JAVASCRIPT FOR OUR PET ADOPT WEBSITE
+// New comment after making changes in the temp branch
+
 async function start() {
   const weatherPromise = await fetch(
     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
