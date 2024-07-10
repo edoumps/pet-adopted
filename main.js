07886d0e449e18329d3a5217859a4c105a674553
edoup <edoumps@gmail.com>
2024-07-10 17:15:20 +0100

    Pet cards filtering done.

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ async function petsArea() {
     console.log(pet.name);
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".pet-card").dataset.species = pet.species;
+
     clone.querySelector("h3").textContent = pet.name;
     clone.querySelector(".pet-description").textContent = pet.description;
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
@@ -72,4 +74,13 @@ function handleButtonClick(e) {
   allButtons.forEach((el) => el.classList.remove("active"));
   // add active class to the clicked button
   e.target.classList.add("active");
+  // Filter the pets
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach((el) => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
